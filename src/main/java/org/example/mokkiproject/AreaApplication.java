92c38e8d1211af3325db76eb46ded40c36bd7f61
Anastasiia Soloveva <anastasiia.soloveva@edu.savonia.fi>
2024-02-22 14:47:23 +0200

    DB_area_added

--- src/main/java/org/example/mokkiproject/AreaApplication.java
+++ src/main/java/org/example/mokkiproject/AreaApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class AreaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AreaApplication.class.getResource("area-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AreaApplication.class.getResource("mokki-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Mokki-project");
         stage.setScene(scene);
--- src/main/java/org/example/mokkiproject/MokkiController.java
+++ src/main/java/org/example/mokkiproject/MokkiController.java
@@ -1,4 +1,46 @@
 package org.example.mokkiproject;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MokkiController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button AlueHakeaButton;
+
+    @FXML
+    private Button AlueLisataButton;
+
+    @FXML
+    private Button AlueMuokataButton;
+
+    @FXML
+    private Button AluePoistaaButton;
+
+    @FXML
+    private Button MokkiHakeaButton;
+
+    @FXML
+    private Button MokkiLisataButton;
+
+    @FXML
+    private Button MokkiMuokataButton;
+
+    @FXML
+    private Button MokkiPoistaaButton;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
 }
